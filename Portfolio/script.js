1024e84799b2cbfebf6a737b43c1fb1e53659bcc
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2018-08-30 08:04:41 +0300

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,51 @@
+var $headerButtonAboutMe =$(".header__button_about-me")
+
+$headerButtonAboutMe.on("click", function(){
+	$("html, body").animate({scrollTop: 0}, 800)
+	offset:112;
+})
+
+
+var $headerButtonEducation =$(".header__button_education")
+
+$headerButtonEducation.on("click", function(){
+	$("html, body").animate({scrollTop: $("#about-me").height()+34}, 400)
+})
+
+
+var $headerButtonSkills =$(".header__button_skills")
+
+$headerButtonSkills.on("click", function(){
+	$("html, body").animate({scrollTop: $("#about-me").height()+
+		$("#education").height()+60}, 400)
+})
+
+
+var $headerButtonExperience =$(".header__button_experience")
+
+$headerButtonExperience.on("click", function(){
+	$("html, body").animate({scrollTop: $("#about-me").height()+
+		$("#education").height()+
+		$("#skills").height()+86}, 400)
+})
+
+
+var $headerButtonProjects =$(".header__button_projects")
+
+$headerButtonProjects.on("click", function(){
+	$("html, body").animate({scrollTop: $("#about-me").height()+
+		$("#education").height()+
+		$("#skills").height()+
+		$("#experience").height()+112}, 400)
+})
+
+
+var $headerButtonContacts =$(".header__button_contacts")
+
+$headerButtonContacts.on("click", function(){
+	$("html, body").animate({scrollTop: $("#about-me").height()+
+		$("#education").height()+
+		$("#skills").height()+
+		$("#experience").height()+
+		$("#projects").height()}+138, 400)
+})
